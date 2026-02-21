47b8d038722a15aec79ae6839e0bd3ff8858cdbb
Anand Sundar <anandsundar96@gmail.com>
2026-02-21 13:11:59 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+This module demonstrates a simple interaction with the OpenAI API to generate a recipe.
+"""
+
+# API Keys: https://platform.openai.com/settings/organization/api-keys
+# Billing:  https://platform.openai.com/settings/organization/billing/overview
+
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+PROMPT = "Give me a very short recipe for a cake please."
+
+response = openai_client.chat.completions.create(
+    model="gpt-5-nano", messages=[{"role": "user", "content": PROMPT}]
+)
+
+answer = response.choices[0].message.content
+
+print(f"\n{answer}\n")
